49be385c5a9e1ae14b8b6bb6768305d96890add7
spoukke <sacha.bernheim@hey.com>
2022-12-15 15:27:15 +0100

    feat: fix init default pod spec

--- controllers/job.go
+++ controllers/job.go
@@ -12,7 +12,7 @@ const (
 	ApplyAction Action = "apply"
 )
 
-func defaultPodSpec() corev1.PodSpec {
+func defaultPodSpec(terraformVersion string, path string, branch string) corev1.PodSpec {
 	return corev1.PodSpec{
 		Containers: []corev1.Container{
 			corev1.Container{
@@ -25,7 +25,7 @@ func defaultPodSpec() corev1.PodSpec {
 
 func getPod(layer *configv1alpha1.TerraformLayer, action Action) corev1.Pod {
 	pod := corev1.Pod{
-		Spec: defaultPodSpec(),
+		Spec: defaultPodSpec(layer.Spec.TerraformVersion, layer.Spec.Path, layer.Spec.Branch),
 	}
 	switch action {
 	case PlanAction:
