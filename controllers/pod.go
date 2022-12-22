dcf799691106803abd0086658f10c6117e09aac7
spoukke <sacha.bernheim@hey.com>
2022-12-22 15:47:14 +0100

    fix: use latest version for runner

--- controllers/pod.go
+++ controllers/pod.go
@@ -48,7 +48,7 @@ func defaultPodSpec(layer *configv1alpha1.TerraformLayer, repository *configv1al
 		Containers: []corev1.Container{
 			{
 				Name:       "runner",
-				Image:      fmt.Sprintf("ghcr.io/padok-team/burrito:%s", "alpha"),
+				Image:      fmt.Sprintf("ghcr.io/padok-team/burrito:%s", "latest"),
 				WorkingDir: "/repository",
 				Args:       []string{"runner", "start"},
 				VolumeMounts: []corev1.VolumeMount{
