3672c5edea5c336d7531e7e73c3337cc55e42e0f
spoukke <sacha.bernheim@hey.com>
2023-02-14 12:31:59 +0100

    fix: use time.Duration for default timers' values

--- cmd/controllers/start.go
+++ cmd/controllers/start.go
@@ -4,6 +4,8 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package controllers
 
 import (
+	"time"
+
 	"github.com/padok-team/burrito/internal/burrito"
 	"github.com/spf13/cobra"
 )
@@ -21,11 +23,15 @@ func buildControllersStartCmd(app *burrito.App) *cobra.Command {
 		},
 	}
 
+	defaultDriftDetectionTimer, _ := time.ParseDuration("20m")
+	defaultOnErrorTimer, _ := time.ParseDuration("1m")
+	defaultWaitActionTimer, _ := time.ParseDuration("1m")
+
 	cmd.Flags().StringSliceVar(&app.Config.Controller.Types, "types", []string{"layer", "repository"}, "list of controllers to start")
 
-	cmd.Flags().DurationVar(&app.Config.Controller.Timers.DriftDetection, "drift-detection-period", "20m", "period between two plans. Must end with s, m or h.")
-	cmd.Flags().DurationVar(&app.Config.Controller.Timers.OnError, "on-error-period", "1m", "period between two runners launch when an error occured. Must end with s, m or h.")
-	cmd.Flags().DurationVar(&app.Config.Controller.Timers.WaitAction, "wait-action-period", "1m", "period between two runners when a layer is locked. Must end with s, m or h.")
+	cmd.Flags().DurationVar(&app.Config.Controller.Timers.DriftDetection, "drift-detection-period", defaultDriftDetectionTimer, "period between two plans. Must end with s, m or h.")
+	cmd.Flags().DurationVar(&app.Config.Controller.Timers.OnError, "on-error-period", defaultOnErrorTimer, "period between two runners launch when an error occured. Must end with s, m or h.")
+	cmd.Flags().DurationVar(&app.Config.Controller.Timers.WaitAction, "wait-action-period", defaultWaitActionTimer, "period between two runners when a layer is locked. Must end with s, m or h.")
 	cmd.Flags().BoolVar(&app.Config.Controller.LeaderElection.Enabled, "leader-election", true, "whether leader election is enabled or not, default to true")
 	cmd.Flags().StringVar(&app.Config.Controller.LeaderElection.ID, "leader-election-id", "6d185457.terraform.padok.cloud", "lease id used for leader election")
 	cmd.Flags().IntVar(&app.Config.Controller.HealthProbeBindAddress, "metrics-bind-address", 8080, "address to bind the metrics server embedded in the controllers")
