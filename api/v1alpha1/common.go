c256cbd17637db6f40853cc3e6b03e3dc49d6128
spoukke <sacha.bernheim@hey.com>
2023-03-23 18:19:06 +0100

    feat: add field to override and merge some

--- api/v1alpha1/common.go
+++ api/v1alpha1/common.go
@@ -9,6 +9,18 @@ type OverrideRunnerSpec struct {
 	Tolerations        []corev1.Toleration           `json:"tolerations,omitempty"`
 	NodeSelector       map[string]string             `json:"nodeSelector,omitempty"`
 	ServiceAccountName string                        `json:"serviceAccountName,omitempty"`
+	Resources          corev1.ResourceRequirements   `json:"resources,omitempty"`
+	Env                []corev1.EnvVar               `json:"env,omitempty"`
+	EnvFrom            []corev1.EnvFromSource        `json:"envFrom,omitempty"`
+	Volumes            []corev1.Volume               `json:"volumes,omitempty"`
+	VolumeMounts       []corev1.VolumeMount          `json:"volumeMounts,omitempty"`
+	Metadata           MetadataOverride              `json:"metadata,omitempty"`
+}
+
+type MetadataOverride struct {
+	Annotations  map[string]string `json:"annotations,omitempty"`
+	Labels       map[string]string `json:"labels,omitempty"`
+	GenerateName string            `json:"generateName,omitempty"`
 }
 
 // +kubebuilder:validation:Enum=dry;autoApply
@@ -54,4 +66,46 @@ func GetTerragruntEnabled(repository *TerraformRepository, layer *TerraformLayer
 		enabled = *layer.Spec.TerraformConfig.TerragruntConfig.Enabled
 	}
 	return enabled
+}
+
+func getOverrideRunenrSpec(repository *TerraformRepository, layer *TerraformLayer) OverrideRunnerSpec {
+	return OverrideRunnerSpec{
+		NodeSelector: mergeMaps(repository.Spec.OverrideRunnerSpec.NodeSelector, layer.Spec.OverrideRunnerSpec.NodeSelector),
+		Metadata: MetadataOverride{
+			Annotations: mergeMaps(repository.Spec.OverrideRunnerSpec.Metadata.Annotations, layer.Spec.OverrideRunnerSpec.Metadata.Annotations),
+			Labels:      mergeMaps(repository.Spec.OverrideRunnerSpec.Metadata.Labels, layer.Spec.OverrideRunnerSpec.Metadata.Labels),
+		},
+		Env: mergeEnvVars(repository.Spec.OverrideRunnerSpec.Env, layer.Spec.OverrideRunnerSpec.Env),
+	}
+
+}
+
+func mergeEnvVars(a, b []corev1.EnvVar) []corev1.EnvVar {
+	result := []corev1.EnvVar{}
+	tempMap := map[string]string{}
+
+	for _, elt := range a {
+		tempMap[elt.Name] = elt.Value
+	}
+	for _, elt := range b {
+		tempMap[elt.Name] = elt.Value
+	}
+
+	for k, v := range tempMap {
+		result = append(result, corev1.EnvVar{Name: k, Value: v})
+	}
+
+	return result
+
+}
+
+func mergeMaps(a, b map[string]string) map[string]string {
+	result := map[string]string{}
+	for k, v := range a {
+		result[k] = v
+	}
+	for k, v := range b {
+		result[k] = v
+	}
+	return result
 }
