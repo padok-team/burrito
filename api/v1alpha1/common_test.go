35a6dec24333757e4ae5371b6fc6620445adc831
spoukke <sacha.bernheim@hey.com>
2023-03-28 16:44:40 +0200

    test: check tolerations merge

--- api/v1alpha1/common_test.go
+++ api/v1alpha1/common_test.go
@@ -3,6 +3,8 @@ package v1alpha1_test
 import (
 	"testing"
 
+	corev1 "k8s.io/api/core/v1"
+
 	configv1alpha1 "github.com/padok-team/burrito/api/v1alpha1"
 )
 
@@ -222,6 +224,95 @@ func TestGetTerragruntEnabled(t *testing.T) {
 			result := configv1alpha1.GetTerragruntEnabled(tc.repository, tc.layer)
 			if tc.expected != result {
 				t.Errorf("different enabled status computed: expected %t go %t", tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestOverrideRunnerSpec(t *testing.T) {
+	tt := []struct {
+		name         string
+		repository   *configv1alpha1.TerraformRepository
+		layer        *configv1alpha1.TerraformLayer
+		expectedSpec configv1alpha1.OverrideRunnerSpec
+	}{
+		{
+			"MergeTolerations",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Tolerations: []corev1.Toleration{
+							{
+								Key:    "only-exists-in-repository",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+							{
+								Key:    "does-not-exists-in-layer",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Tolerations: []corev1.Toleration{
+							{
+								Key:    "does-not-exists-in-layer",
+								Value:  "false",
+								Effect: "NoExecute",
+							},
+							{
+								Key:    "only-exists-in-layer",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				Tolerations: []corev1.Toleration{
+					{
+						Key:    "only-exists-in-repository",
+						Value:  "true",
+						Effect: "NoSchedule",
+					},
+					{
+						Key:    "does-not-exists-in-layer",
+						Value:  "false",
+						Effect: "NoExecute",
+					},
+					{
+						Key:    "only-exists-in-layer",
+						Value:  "true",
+						Effect: "NoSchedule",
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			result := configv1alpha1.GetOverrideRunnerSpec(tc.repository, tc.layer)
+
+			if len(result.Tolerations) != len(tc.expectedSpec.Tolerations) {
+				t.Errorf("different tolerations size: got %d expected %d", len(result.Tolerations), len(tc.expectedSpec.Tolerations))
+			}
+			for i, tol := range result.Tolerations {
+				if tol.Key != result.Tolerations[i].Key {
+					t.Errorf("different tolerations key: got %s expected %s", result.Tolerations[i].Key, tol.Key)
+				}
+				if tol.Value != result.Tolerations[i].Value {
+					t.Errorf("different tolerations value: got %s expected %s", result.Tolerations[i].Value, tol.Value)
+				}
+				if tol.Effect != result.Tolerations[i].Effect {
+					t.Errorf("different tolerations effect: got %s expected %s", result.Tolerations[i].Effect, tol.Effect)
+				}
 			}
 		})
 	}
