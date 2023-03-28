e62786e149cb5d243e895f27955f864fcf647cfd
spoukke <sacha.bernheim@hey.com>
2023-03-28 18:10:50 +0200

    test: add for merge image, pullsecrets and nodeselector

--- api/v1alpha1/common_test.go
+++ api/v1alpha1/common_test.go
@@ -294,12 +294,253 @@ func TestOverrideRunnerSpec(t *testing.T) {
 				},
 			},
 		},
+		{
+			"TolerationsOnlyInrepository",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Tolerations: []corev1.Toleration{
+							{
+								Key:    "only-exists-in-repository",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+							{
+								Key:    "does-not-exists-in-layer",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{},
+			configv1alpha1.OverrideRunnerSpec{
+				Tolerations: []corev1.Toleration{
+					{
+						Key:    "only-exists-in-repository",
+						Value:  "true",
+						Effect: "NoSchedule",
+					},
+					{
+						Key:    "does-not-exists-in-layer",
+						Value:  "true",
+						Effect: "NoSchedule",
+					},
+				},
+			},
+		},
+		{
+			"TolerationsOnlyInLayer",
+			&configv1alpha1.TerraformRepository{},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Tolerations: []corev1.Toleration{
+							{
+								Key:    "does-not-exists-in-layer",
+								Value:  "false",
+								Effect: "NoExecute",
+							},
+							{
+								Key:    "only-exists-in-layer",
+								Value:  "true",
+								Effect: "NoSchedule",
+							},
+						},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				Tolerations: []corev1.Toleration{
+					{
+						Key:    "does-not-exists-in-layer",
+						Value:  "false",
+						Effect: "NoExecute",
+					},
+					{
+						Key:    "only-exists-in-layer",
+						Value:  "true",
+						Effect: "NoSchedule",
+					},
+				},
+			},
+		},
+		{
+			"ChooseImageNotSpecified",
+			&configv1alpha1.TerraformRepository{},
+			&configv1alpha1.TerraformLayer{},
+			configv1alpha1.OverrideRunnerSpec{},
+		},
+		{
+			"ChooseRepositoryImage",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Image: "test",
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{},
+			configv1alpha1.OverrideRunnerSpec{
+				Image: "test",
+			},
+		},
+		{
+			"ChooseLayerImage",
+			&configv1alpha1.TerraformRepository{},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Image: "test",
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				Image: "test",
+			},
+		},
+		{
+			"OverrideRepositoryImageInLayer",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Image: "test",
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						Image: "overrdie",
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				Image: "overrdie",
+			},
+		},
+		{
+			"PullSecretsInRepository",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						ImagePullSecrets: []corev1.LocalObjectReference{
+							{Name: "test"},
+						},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{},
+			configv1alpha1.OverrideRunnerSpec{
+				ImagePullSecrets: []corev1.LocalObjectReference{
+					{Name: "test"},
+				},
+			},
+		},
+		{
+			"PullSecretsInLayer",
+			&configv1alpha1.TerraformRepository{},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						ImagePullSecrets: []corev1.LocalObjectReference{
+							{Name: "test"},
+						},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				ImagePullSecrets: []corev1.LocalObjectReference{
+					{Name: "test"},
+				},
+			},
+		},
+		{
+			"PullSecretsInBoth",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						ImagePullSecrets: []corev1.LocalObjectReference{
+							{Name: "repo"},
+							{Name: "common"},
+						},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						ImagePullSecrets: []corev1.LocalObjectReference{
+							{Name: "layer"},
+							{Name: "common"},
+						},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				ImagePullSecrets: []corev1.LocalObjectReference{
+					{Name: "repo"},
+					{Name: "common"},
+					{Name: "layer"},
+				},
+			},
+		},
+		{
+			"MergeNodeSelector",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						NodeSelector: map[string]string{"only-in-repo": "true", "exists-in-both": "false"},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						NodeSelector: map[string]string{"exists-in-both": "true", "only-in-layer": "true"},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				NodeSelector: map[string]string{"only-in-repo": "true", "exists-in-both": "true", "only-in-layer": "true"},
+			},
+		},
+		{
+			"NodeSelectorOnlyInRepo",
+			&configv1alpha1.TerraformRepository{
+				Spec: configv1alpha1.TerraformRepositorySpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						NodeSelector: map[string]string{"only-in-repo": "true", "exists-in-both": "false"},
+					},
+				},
+			},
+			&configv1alpha1.TerraformLayer{},
+			configv1alpha1.OverrideRunnerSpec{
+				NodeSelector: map[string]string{"only-in-repo": "true", "exists-in-both": "false"},
+			},
+		},
+		{
+			"NodeSelectorOnlyInLayer",
+			&configv1alpha1.TerraformRepository{},
+			&configv1alpha1.TerraformLayer{
+				Spec: configv1alpha1.TerraformLayerSpec{
+					OverrideRunnerSpec: configv1alpha1.OverrideRunnerSpec{
+						NodeSelector: map[string]string{"exists-in-both": "true", "only-in-layer": "true"},
+					},
+				},
+			},
+			configv1alpha1.OverrideRunnerSpec{
+				NodeSelector: map[string]string{"exists-in-both": "true", "only-in-layer": "true"},
+			},
+		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			result := configv1alpha1.GetOverrideRunnerSpec(tc.repository, tc.layer)
 
+			// Check Tolerations
 			if len(result.Tolerations) != len(tc.expectedSpec.Tolerations) {
 				t.Errorf("different tolerations size: got %d expected %d", len(result.Tolerations), len(tc.expectedSpec.Tolerations))
 			}
@@ -312,6 +553,31 @@ func TestOverrideRunnerSpec(t *testing.T) {
 				}
 				if tol.Effect != result.Tolerations[i].Effect {
 					t.Errorf("different tolerations effect: got %s expected %s", result.Tolerations[i].Effect, tol.Effect)
+				}
+			}
+
+			// Check IMage
+			if tc.expectedSpec.Image != result.Image {
+				t.Errorf("different images: got %s expect %s", result.Image, tc.expectedSpec.Image)
+			}
+
+			// Check ImagePullSecrets
+			if len(result.ImagePullSecrets) != len(tc.expectedSpec.ImagePullSecrets) {
+				t.Errorf("differents image pull secrets size: got %d expected %d", len(result.ImagePullSecrets), len(tc.expectedSpec.ImagePullSecrets))
+			}
+			for i, secret := range result.ImagePullSecrets {
+				if secret.Name != tc.expectedSpec.ImagePullSecrets[i].Name {
+					t.Errorf("different image pull secret names: got %s expected %s", secret.Name, tc.expectedSpec.ImagePullSecrets[i].Name)
+				}
+			}
+
+			//Check NodeSelector
+			if len(tc.expectedSpec.NodeSelector) != len(result.NodeSelector) {
+				t.Errorf("different size of node selector: got %d expected %d", len(result.NodeSelector), len(tc.expectedSpec.NodeSelector))
+			}
+			for k, v := range result.NodeSelector {
+				if tc.expectedSpec.NodeSelector[k] != v {
+					t.Errorf("different node selector value for label %s: got %s expected %s", k, v, tc.expectedSpec.NodeSelector[k])
 				}
 			}
 		})
