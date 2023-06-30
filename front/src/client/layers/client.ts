73db680088510e1dd7a6af57f6b4be2e8f339968
Jérémie Marniquet <jeremiem@padok.fr>
2023-06-30 17:49:47 +0200

    comment

--- front/src/client/layers/client.ts
+++ front/src/client/layers/client.ts
@@ -24,34 +24,34 @@ export async function fetchLayer(
   return response.data;
 }
 
-export async function fetchMocLayer(
-  namespace: string,
-  name: string,
-): Promise<Layer> {
-  const resource1: Resource = {
-    address: 'aws_terraform_ressource.toto',
-    type: 'aws_terraform_ressource',
-    status: 'green',
-    depends_on: ['lucas', 'Jerem', 'Thibaut'],
-  };
-  const resource2: Resource = {
-    address: 'aws_terraform_ressource.tata',
-    type: 'aws_terraform_ressource',
-    status: 'blue',
-    depends_on: [],
-  };
-  const layer: Layer = {
-    id: 'id_layer_1',
-    name: 'layer_name',
-    namespace: 'mynamespace',
-    repoUrl: 'layer_url',
-    path: 'layer_path',
-    branch: 'branch',
-    status: LayerStatus.Idle,
-    lastPlanCommit: 'plan_commit',
-    lastApplyCommit: 'apply_commit',
-    lastRelevantCommit: 'relevant_commit',
-    resources: [resource1, resource2],
-  };
-  return layer;
-}
+// export async function fetchMocLayer(
+//   namespace: string,
+//   name: string,
+// ): Promise<Layer> {
+//   const resource1: Resource = {
+//     address: 'aws_terraform_ressource.toto',
+//     type: 'aws_terraform_ressource',
+//     status: 'green',
+//     depends_on: ['lucas', 'Jerem', 'Thibaut'],
+//   };
+//   const resource2: Resource = {
+//     address: 'aws_terraform_ressource.tata',
+//     type: 'aws_terraform_ressource',
+//     status: 'blue',
+//     depends_on: [],
+//   };
+//   const layer: Layer = {
+//     id: 'id_layer_1',
+//     name: 'layer_name',
+//     namespace: 'mynamespace',
+//     repoUrl: 'layer_url',
+//     path: 'layer_path',
+//     branch: 'branch',
+//     status: LayerStatus.Idle,
+//     lastPlanCommit: 'plan_commit',
+//     lastApplyCommit: 'apply_commit',
+//     lastRelevantCommit: 'relevant_commit',
+//     resources: [resource1, resource2],
+//   };
+//   return layer;
+// }
