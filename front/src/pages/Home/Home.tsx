e3af2a00b8ec8f5776ca9228094a5820f9c6d746
Jérémie Marniquet <jeremiem@padok.fr>
2023-06-30 14:14:11 +0200

    feat: add react queyr

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import { Container } from './Home.style';
 import BaseLayout from 'layouts/BaseLayout';
@@ -9,9 +8,7 @@ interface Props {}
 const Home: React.FC<Props> = () => {
   return (
     <BaseLayout>
-      <Container>
-        <FormattedMessage id="title" />
-      </Container>
+      <Container></Container>
     </BaseLayout>
   );
 };
