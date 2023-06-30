39bdc3d722632209ee672bf3be01f22da3c3d171
Luca CORRIERI <lucac@padok.fr>
2023-06-30 17:20:57 +0200

    fix(server): hardcode redis connection

--- internal/server/routes/commons.go
+++ internal/server/routes/commons.go
@@ -1,16 +1,16 @@
 package routes
 
 import (
+	configv1alpha1 "github.com/padok-team/burrito/api/v1alpha1"
 	"github.com/padok-team/burrito/internal/burrito/config"
 	"github.com/padok-team/burrito/internal/storage"
 	"github.com/padok-team/burrito/internal/storage/redis"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	configv1alpha1 "github.com/padok-team/burrito/api/v1alpha1"
+	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type LayerClient struct {
@@ -37,6 +37,9 @@ func (l *LayerClient) Init() error {
 	}
 	l.client = cl
 
+	// TODO: Fix configuration
+	l.config.Redis.URL = "burrito-redis:6379"
+	log.Infof("Connecting to redis at %s", l.config.Redis.URL)
 	l.storage = redis.New(l.config.Redis.URL, l.config.Redis.Password, l.config.Redis.Database)
 	return nil
 }
