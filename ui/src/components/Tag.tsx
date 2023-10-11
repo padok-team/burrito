b11f81ea0c24a54a7cefe7829c7eae937bc59464
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-11 18:37:27 +0200

    feat(ui): add tag component

---
+++ ui/src/components/Tag.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+interface TagProps {
+  variant: "success" | "warning" | "error" | "disabled";
+}
+
+const Tag: React.FC<TagProps> = ({ variant }) => {
+  const variantClasses = {
+    success: `bg-status-success-default
+    text-nuances-black`,
+    warning: `bg-status-warning-default
+    text-nuances-black`,
+    error: `bg-status-error-default
+    text-nuances-white`,
+    disabled: `bg-nuances-50
+    text-nuances-200`,
+  };
+
+  const getContent = () => {
+    switch (variant) {
+      case "success":
+        return "OK";
+      case "warning":
+        return "OutOfSync";
+      case "error":
+        return "Error";
+      case "disabled":
+        return "Disabled";
+    }
+  };
+
+  return (
+    <div
+      className={`flex
+        px-3 py-1
+        items-center
+        gap-1
+        rounded-full
+        text-sm
+        font-semibold
+        leading-5
+        ${variantClasses[variant]}`}
+    >
+      {getContent()}
+    </div>
+  );
+};
+
+export default Tag;
