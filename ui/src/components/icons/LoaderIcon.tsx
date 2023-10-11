4e429239618bdfd7e6051634697ed7b3f3429da2
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-11 17:35:27 +0200

    feat(ui): add arrow icons

--- ui/src/components/icons/LoaderIcon.tsx
+++ ui/src/components/icons/LoaderIcon.tsx
@@ -1,7 +1,7 @@
 import { SVGProps } from "react";
 
 const LoaderIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg {...props}>
+  <svg height={24} width={24} {...props}>
     <path d="M5.474 5.474c-.301-.301-.792-.303-1.068.02a10 10 0 1 0 7.111-3.482c-.425.02-.724.409-.67.83.053.423.438.719.863.704A8.459 8.459 0 1 1 5.5 6.587c.272-.327.275-.812-.026-1.113Z" />
   </svg>
 );
