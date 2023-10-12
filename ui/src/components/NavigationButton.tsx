b439c763b46c23ec863d290ee49313b0f28a1801
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-12 11:59:51 +0200

    feat(ui): add navigation button component

---
+++ ui/src/components/NavigationButton.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface NavigationButtonProps {
+  variant?: "light" | "dark";
+  icon: React.ReactNode;
+  onClick?: () => void;
+}
+
+const NavigationButton: React.FC<NavigationButtonProps> = ({
+  variant,
+  icon,
+  onClick,
+}) => {
+  const variantClasses = {
+    light: `bg-primary-400
+      hover:bg-primary-500
+      fill-primary-600`,
+
+    dark: `bg-nuances-black
+      hover:bg-nuances-400
+      fill-nuances-white`,
+  };
+
+  variant = variant ?? "dark";
+
+  return (
+    <button
+      className={`flex
+        justify-center
+        items-center
+        w-8
+        h-8
+        flex-shrink
+        rounded-lg
+        ${variantClasses[variant]}`}
+      onClick={onClick}
+    >
+      {icon}
+    </button>
+  );
+};
+
+export default NavigationButton;
