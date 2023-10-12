f361fcd8782f2c8582266714c3c12f9d1d643464
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-12 11:59:13 +0200

    feat(ui): add new icons

---
+++ ui/src/assets/icons/MultiplyIcon.tsx
@@ -0,0 +1,8 @@
+import { SVGProps } from "react";
+
+const MultiplyIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg height={24} width={24} {...props}>
+    <path d="m13.233 12 5.508-5.501A.878.878 0 0 0 17.5 5.257l-5.5 5.51-5.5-5.51A.878.878 0 1 0 5.26 6.5l5.508 5.5-5.508 5.5a.874.874 0 0 0 0 1.242.872.872 0 0 0 1.241 0l5.5-5.509 5.5 5.51a.872.872 0 0 0 1.241 0 .876.876 0 0 0 0-1.242L13.233 12Z" />
+  </svg>
+);
+export default MultiplyIcon;
