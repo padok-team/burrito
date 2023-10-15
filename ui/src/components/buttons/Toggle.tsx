a4e4fef9532aa1dd26d6415e595a6d9ce0da4b56
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-15 03:51:37 +0200

    feat(ui): add toggle component

---
+++ ui/src/components/buttons/Toggle.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+import AvocadoOn from "@/assets/avocado/AvocadoOn";
+import AvocadoOff from "@/assets/avocado/AvocadoOff";
+import AvocadoSeed from "@/assets/avocado/AvocadoSeed";
+
+export interface ToggleProps {
+  className?: string;
+  checked?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ className, checked, onChange }) => {
+  const [internalChecked, setInternalChecked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInternalChecked(event.target.checked);
+  };
+
+  return (
+    <label className={twMerge(`relative rotate-180 cursor-pointer`, className)}>
+      <AvocadoOn
+        className={`relative
+          top-0
+          left-0
+          transition-all
+          ease-in-out
+          duration-500
+          ${
+            checked ?? internalChecked ? "opacity-100" : "opacity-0 delay-150"
+          }`}
+        height={32}
+        width={48}
+      />
+      <AvocadoOff
+        className={`absolute
+          top-0
+          left-0
+          transition-all
+          ease-in-out
+          duration-500
+          ${
+            checked ?? internalChecked ? "opacity-0 delay-150" : "opacity-100"
+          }`}
+        height={32}
+        width={48}
+      />
+      <AvocadoSeed
+        className={`absolute
+          top-0
+          left-0
+          transition-all
+          duration-500
+          ${checked ?? internalChecked ? "rotate-90" : "translate-x-[22px] "}`}
+        height={32}
+        width={32}
+      />
+      <input
+        type="checkbox"
+        className="hidden"
+        checked={checked ?? internalChecked}
+        onChange={onChange ?? handleChange}
+      />
+    </label>
+  );
+};
+
+export default Toggle;
