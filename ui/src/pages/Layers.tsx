fcfe1ea826ab80cef06af53522b87aa008248cf1
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-16 18:22:42 +0200

    feat(ui): make layers page scrollable

--- ui/src/pages/Layers.tsx
+++ ui/src/pages/Layers.tsx
@@ -13,48 +13,52 @@ import BarsIcon from "@/assets/icons/BarsIcon";
 
 const Layers: React.FC = () => {
   return (
-    <div className="flex">
+    <div className="flex bg-primary-100">
       <NavigationBar />
-      <div className="flex flex-col flex-grow p-6 gap-6">
-        <div className="flex justify-between">
-          <h1 className="text-[32px] font-extrabold leading-[130%]">Layers</h1>
-          <Button>Refresh layers</Button>
-        </div>
-        <Input
-          className="w-full"
-          placeholder="Search into layers"
-          leftIcon={<SearchIcon />}
-        />
-        <div className="flex flex-row items-center justify-between gap-8">
-          <div className="flex flex-row items-center gap-4">
-            <span className="text-base font-semibold text-nuances-black">
-              267 layers
-            </span>
-            <span className="text-base font-medium text-primary-600">|</span>
-            <span className="text-base font-medium text-primary-600">
-              Filter by
-            </span>
-            <div className="flex flex-row items-center gap-2">
-              <div className="flex items-center h-8 p-2 rounded-lg bg-primary-400">
-                State
-              </div>
-              <div className="flex items-center h-8 p-2 rounded-lg bg-primary-400">
-                Repository
-              </div>
-            </div>
-            <div className="flex flex-row items-center gap-[7px]">
-              <span className="text-sm font-medium text-nuances-black">
-                Show open PR
+      <div className="flex flex-col flex-grow h-screen gap-3 overflow-auto">
+        <div className="sticky top-0 flex flex-col p-6 pb-3 gap-6 bg-primary-100">
+          <div className="flex justify-between">
+            <h1 className="text-[32px] font-extrabold leading-[130%]">
+              Layers
+            </h1>
+            <Button>Refresh layers</Button>
+          </div>
+          <Input
+            className="w-full"
+            placeholder="Search into layers"
+            leftIcon={<SearchIcon />}
+          />
+          <div className="flex flex-row items-center justify-between gap-8">
+            <div className="flex flex-row items-center gap-4">
+              <span className="text-base font-semibold text-nuances-black">
+                267 layers
               </span>
-              <Toggle defaultChecked />
+              <span className="text-base font-medium text-primary-600">|</span>
+              <span className="text-base font-medium text-primary-600">
+                Filter by
+              </span>
+              <div className="flex flex-row items-center gap-2">
+                <div className="flex items-center h-8 p-2 rounded-lg bg-primary-400">
+                  State
+                </div>
+                <div className="flex items-center h-8 p-2 rounded-lg bg-primary-400">
+                  Repository
+                </div>
+              </div>
+              <div className="flex flex-row items-center gap-[7px]">
+                <span className="text-sm font-medium text-nuances-black">
+                  Show open PR
+                </span>
+                <Toggle defaultChecked />
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <NavigationButton icon={<AppsIcon />} />
+              <NavigationButton icon={<BarsIcon />} variant="light" />
             </div>
           </div>
-          <div className="flex flex-row items-center gap-2">
-            <NavigationButton icon={<AppsIcon />} />
-            <NavigationButton icon={<BarsIcon />} variant="light" />
-          </div>
         </div>
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-6">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] p-6 pt-3 gap-6">
           <Card
             title="fail-terragrunt"
             isRunning
