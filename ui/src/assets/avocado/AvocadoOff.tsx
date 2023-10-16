42b913e932da8e220fcf8c624c91c831d1788ba8
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-16 10:21:33 +0200

    fix(ui): fix bugged toggle SVGs

--- ui/src/assets/avocado/AvocadoOff.tsx
+++ ui/src/assets/avocado/AvocadoOff.tsx
@@ -20,37 +20,19 @@ const AvocadoOff = (props: SVGProps<SVGSVGElement>) => (
       strokeWidth={10}
       d="m184.117 123.393-.029.003-.028.004c-22.646 2.932-44.598 9.442-64.914 19.257-11.524 5.323-24.335 7.829-37.216 7.265-12.896-.565-25.403-4.187-36.337-10.499-10.933-6.312-19.914-15.095-26.128-25.487-6.21-10.389-9.46-22.055-9.465-33.897 0-1.252.036-2.512.107-3.778.672-11.675 4.492-23.029 11.135-33.013 6.646-9.988 15.912-18.292 26.97-24.116 11.06-5.825 23.54-8.97 36.283-9.126 12.743-.156 25.309 2.683 36.533 8.237l.024.012.024.011c20.637 9.939 43.036 16.278 66.096 18.713l2.037.24c11.366 1.336 21.751 6.484 29.233 14.39 7.474 7.898 11.544 17.999 11.544 28.396 0 10.398-4.07 20.499-11.544 28.396-7.482 7.907-17.867 13.055-29.233 14.391l-.002.001-5.09.6Z"
     />
-    <g filter="url(#a)">
-      <path
-        fill="#C2D1DF"
-        fillRule="evenodd"
-        d="M80 115c19.33 0 35-15.67 35-35S99.33 45 80 45 45 60.67 45 80s15.67 35 35 35Z"
-        clipRule="evenodd"
-      />
-    </g>
-    <defs>
-      <filter
-        id="a"
-        width={70}
-        height={70}
-        x={45}
-        y={45}
-        colorInterpolationFilters="sRGB"
-        filterUnits="userSpaceOnUse"
-      >
-        <feFlood floodOpacity={0} result="BackgroundImageFix" />
-        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-        <feColorMatrix
-          in="SourceAlpha"
-          result="hardAlpha"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-        />
-        <feOffset dx={10} dy={10} />
-        <feComposite in2="hardAlpha" k2={-1} k3={1} operator="arithmetic" />
-        <feColorMatrix values="0 0 0 0 0.700972 0 0 0 0 0.760814 0 0 0 0 0.816667 0 0 0 1 0" />
-        <feBlend in2="shape" result="effect1_innerShadow_12812_4083" />
-      </filter>
-    </defs>
+    <circle
+      cx={35}
+      cy={35}
+      r={35}
+      fill="#B3C2D0"
+      transform="matrix(-1 0 0 1 115 45)"
+    />
+    <path
+      fill="#C2D1DF"
+      fillRule="evenodd"
+      d="M80 115c-19.33 0-35-15.67-35-35 0-7.108 2.119-13.72 5.759-19.241A34.836 34.836 0 0 1 70 55c19.33 0 35 15.67 35 35a34.84 34.84 0 0 1-5.759 19.241A34.838 34.838 0 0 1 80 115Z"
+      clipRule="evenodd"
+    />
   </svg>
 );
 
