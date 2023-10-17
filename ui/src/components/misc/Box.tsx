5cc625e65d919f8542a2465a5c25be9b8e200dd5
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-17 11:54:44 +0200

    feat(ui): add box component

---
+++ ui/src/components/misc/Box.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+export interface BoxProps {
+  className?: string;
+  variant?: "light" | "dark";
+  children: React.ReactNode;
+}
+
+const Box: React.FC<BoxProps> = ({
+  className,
+  variant = "light",
+  children,
+}) => {
+  const styles = {
+    light: `bg-nuances-white
+      shadow-light`,
+    dark: `bg-nuances-black
+      shadow-dark`,
+  };
+
+  return (
+    <div className={twMerge(`flex rounded-lg ${styles[variant]}`, className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Box;
