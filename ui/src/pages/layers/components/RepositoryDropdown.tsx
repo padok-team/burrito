bc9afa31b0461b5579b3e6c41d8bbb1ffa5c53bf
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-19 18:48:51 +0200

    feat(ui): add dark mode support for layers page components

--- ui/src/pages/layers/components/RepositoryDropdown.tsx
+++ ui/src/pages/layers/components/RepositoryDropdown.tsx
@@ -4,17 +4,40 @@ import Box from "@/components/misc/Box";
 import Input from "@/components/inputs/Input";
 import Checkbox from "@/components/checkboxes/Checkbox";
 
-const RepositoryDropdown: React.FC = () => {
+export interface RepositoryDropdownProps {
+  variant?: "light" | "dark";
+}
+
+const RepositoryDropdown: React.FC<RepositoryDropdownProps> = ({ variant }) => {
   return (
-    <Box className="flex-col items-center justify-center gap-2">
+    <Box
+      variant={variant}
+      className="flex-col items-center justify-center gap-2"
+    >
       <span className="self-start mx-4 mt-2">Repository</span>
-      <hr className="h-[1px] w-full bg-primary-600" />
-      <Input className="w-[200px] mx-2" placeholder="Search repository" />
-      <hr className="h-[1px] w-full bg-primary-600" />
+      <hr
+        className={`
+          h-[1px]
+          w-full
+          ${variant === "light" ? "bg-primary-600" : "bg-nuances-300"}
+        `}
+      />
+      <Input
+        variant={variant}
+        className="w-[200px] mx-2"
+        placeholder="Search repository"
+      />
+      <hr
+        className={`
+          h-[1px]
+          w-full
+          ${variant === "light" ? "bg-primary-600" : "bg-nuances-300"}
+        `}
+      />
       <div className="flex flex-col self-start mx-4 mb-2 gap-2">
-        <Checkbox label="Burrito" />
-        <Checkbox label="Burrito-1" />
-        <Checkbox label="Burrito-2" />
+        <Checkbox variant={variant} label="Burrito" />
+        <Checkbox variant={variant} label="Burrito-1" />
+        <Checkbox variant={variant} label="Burrito-2" />
       </div>
     </Box>
   );
--- ui/src/pages/layers/components/StateDropdown.tsx
+++ ui/src/pages/layers/components/StateDropdown.tsx
@@ -3,15 +3,28 @@ import React from "react";
 import Box from "@/components/misc/Box";
 import Checkbox from "@/components/checkboxes/Checkbox";
 
-const StateDropdown: React.FC = () => {
+export interface StateDropdownProps {
+  variant?: "light" | "dark";
+}
+
+const StateDropdown: React.FC<StateDropdownProps> = ({ variant }) => {
   return (
-    <Box className="flex-col items-center justify-center gap-2">
+    <Box
+      variant={variant}
+      className="flex-col items-center justify-center gap-2"
+    >
       <span className="self-start mx-4 mt-2">State</span>
-      <hr className="h-[1px] w-full bg-primary-600" />
+      <hr
+        className={`
+          h-[1px]
+          w-full
+          ${variant === "light" ? "bg-primary-600" : "bg-nuances-300"}
+        `}
+      />
       <div className="flex flex-col self-start mx-4 mb-2 gap-2">
-        <Checkbox label="OK" />
-        <Checkbox label="OutOfSync" />
-        <Checkbox label="Error" />
+        <Checkbox variant={variant} label="OK" />
+        <Checkbox variant={variant} label="OutOfSync" />
+        <Checkbox variant={variant} label="Error" />
       </div>
     </Box>
   );
