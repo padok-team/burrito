eb565fd04dc5bc9798ac4e750c0f4d534279a331
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-23 14:51:07 +0200

    feat(ui): configure react-query

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import ThemeProvider from "@/contexts/ThemeContext";
 
 import Layers from "@/pages/layers/Layers";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ThemeProvider>
-      <Layers />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Layers />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
