f5fd260568cdb33d86ae476d6526e94985b19804
Marc-Antoine Godde <marcantoineg@padok.fr>
2023-10-25 10:44:14 +0200

    feat(ui): improve login page and add dark version

--- ui/src/pages/login/Login.tsx
+++ ui/src/pages/login/Login.tsx
@@ -9,49 +9,112 @@ import SocialButton from "@/components/buttons/SocialButton";
 
 import Burrito from "@/assets/illustrations/Burrito";
 import EyeSlashIcon from "@/assets/icons/EyeSlashIcon";
-import CoverLight from "@/assets/covers/cover-light.png";
-import CoverDark from "@/assets/covers/cover-dark.png";
+// import CoverLight from "@/assets/covers/cover-light.png";
+// import CoverDark from "@/assets/covers/cover-dark.png";
 
 const Login: React.FC = () => {
   const { theme } = useContext(ThemeContext);
   const navigate = useNavigate();
   return (
     <div className="flex h-screen">
-      <div className="inline-block justify-center p-6 bg-nuances-white overflow-y-scroll w-[500px] min-w-[500px]">
+      <div
+        className={`
+          inline-block
+          justify-center
+          overflow-y-scroll
+          w-[500px]
+          min-w-[500px]
+          p-6
+          ${theme === "light" ? "bg-nuances-white" : "bg-nuances-black"}
+        `}
+      >
         <div className="flex items-center justify-center">
-          <div className="flex flex-col items-start justify-center gap-10">
+          <div className="flex flex-col items-start justify-center min-w-[300px] gap-10">
             <div className="flex flex-col items-start gap-2">
               <Burrito height={64} width={64} />
-              <span className="text-3xl font-extrabold">
+              <span
+                className={`
+                  text-3xl
+                  font-extrabold
+                  ${
+                    theme === "light"
+                      ? "text-nuances-black"
+                      : "text-nuances-white"
+                  }
+                `}
+              >
                 Welcome to Burrito
               </span>
             </div>
             <div className="flex flex-col items-center justify-center gap-8">
               <Input
-                className="w-[300px]"
+                className="min-w-full"
+                variant={theme}
                 placeholder="Your email"
                 label="Email"
               />
               <Input
-                className="w-full"
+                className="min-w-full"
+                variant={theme}
                 placeholder="Your password"
                 label="Password"
                 rightIcon={<EyeSlashIcon />}
               />
-              <Button className="w-full" onClick={() => navigate("/")}>
+              <Button
+                className="min-w-full"
+                variant={theme === "light" ? "primary" : "secondary"}
+                onClick={() => navigate("/")}
+              >
                 Login
               </Button>
-              <div className="flex flex-row items-center w-full gap-4">
+              <div
+                className={`
+                  flex
+                  flex-row
+                  items-center
+                  min-w-full
+                  gap-4
+                  ${
+                    theme === "light"
+                      ? "text-nuances-black border-nuances-black"
+                      : "text-nuances-white border-nuances-white"
+                  }
+                `}
+              >
                 <hr className="w-full" />
                 <span>OR</span>
                 <hr className="w-full" />
               </div>
-              <div className="flex flex-col items-center justify-center gap-4">
-                <SocialButton variant="github" onClick={() => navigate("/")} />
-                <SocialButton variant="gitlab" onClick={() => navigate("/")} />
+              <div className="flex flex-col items-center justify-center min-w-full gap-4">
+                <SocialButton
+                  className="w-full"
+                  variant="github"
+                  onClick={() => navigate("/")}
+                />
+                <SocialButton
+                  className="w-full"
+                  variant="gitlab"
+                  onClick={() => navigate("/")}
+                />
               </div>
             </div>
-            <div className="flex flex-row items-center justify-center gap-1 bg-primary-400 p-6 w-full rounded-lg">
+            <div
+              className={`
+                flex
+                flex-row
+                items-center
+                justify-center
+                min-w-full
+                gap-1
+                p-6
+                rounded-lg
+                ${
+                  theme === "light"
+                    ? "bg-primary-400 text-nuances-black"
+                    : "bg-nuances-400 text-nuances-50"
+                }
+              `}
+            >
               <span className="text-base font-normal">
                 Don't have an account ?
               </span>
@@ -60,8 +123,25 @@ const Login: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="relative flex flex-grow flex-col pt-20 px-16 bg-background-light gap-6 overflow-hidden">
-        <div className="flex flex-col">
+      <div
+        className={`
+          relative
+          flex
+          flex-col
+          w-[calc(100%_-_500px)]
+          pt-20
+          px-16
+          gap-6
+          bg-background-dark
+        `}
+      >
+        <div
+          className={`
+            flex
+            flex-col
+            ${theme === "light" ? "text-nuances-black" : "text-nuances-white"}
+          `}
+        >
           <span className="text-5xl font-extrabold">Burrito is a TACoS</span>
           <span className="text-base font-medium">
             Monitor the status of your layers and their impacts on your project.
