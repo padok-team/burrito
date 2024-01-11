fca899a69e9abbcb4b73e8122e06103aba5e5e91
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-01-12 00:38:54 +0100

    feat(ui): first implementation of NavigationLink component

---
+++ ui/src/components/navigation/NavigationLink.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+import { Link, LinkProps } from "react-router-dom";
+
+export interface NavigationLinkProps {
+  className?: string;
+  value: React.ReactNode;
+  to: LinkProps["to"];
+  disabled?: boolean;
+}
+
+const NavigationLink: React.FC<NavigationLinkProps> = ({
+  className,
+  value,
+  to,
+  disabled,
+}) => {
+  return !disabled ? (
+    <Link to={to}>
+      <div
+        className={twMerge(
+          `relative
+          text-blue-500
+          fill-blue-500
+          hover:text-blue-400
+          hover:fill-blue-400
+          duration-500
+          hover:duration-500
+          after:absolute
+          after:-bottom-[3px]
+          after:left-0
+          after:w-full
+          after:h-[3px]
+          after:bg-blue-500
+          after:rounded-sm
+          after:scale-x-0
+          after:opacity-0
+          after:origin-bottom-left
+          after:transition-all
+          after:duration-500
+          hover:after:scale-x-100
+          hover:after:opacity-100
+          hover:after:rounded-sm
+          hover:after:bg-blue-400
+          hover:after:transition-all
+          hover:after:duration-500`,
+          className
+        )}
+      >
+        {value}
+      </div>
+    </Link>
+  ) : (
+    <div
+      className={twMerge(
+        `text-nuances-200
+        fill-nuances-200`,
+        className
+      )}
+    >
+      {value}
+    </div>
+  );
+};
+
+export default NavigationLink;
