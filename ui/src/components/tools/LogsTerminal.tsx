29658f3fa6bfa7e1273f5c951956db0703ee385f
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-01-18 18:09:29 +0100

    feat(ui): extend logs terminal

--- ui/src/components/tools/LogsTerminal.tsx
+++ ui/src/components/tools/LogsTerminal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { twMerge } from "tailwind-merge";
 
 import Dropdown from "@/components/core/Dropdown";
+import AttemptButton from "@/components/buttons/AttemptButton";
 import CopyIcon from "@/assets/icons/CopyIcon";
 import DownloadAltIcon from "@/assets/icons/DownloadAltIcon";
 
@@ -21,10 +22,12 @@ const LogsTerminal: React.FC<LogsTerminalProps> = ({
   const styles = {
     light: `bg-nuances-50
       text-nuances-black
-      fill-nuances-black`,
+      fill-nuances-black
+      border-primary-500`,
     dark: `bg-nuances-400
       text-nuances-50
-      fill-nuances-50`,
+      fill-nuances-50
+      border-nuances-black`,
   };
 
   return (
@@ -47,12 +50,33 @@ const LogsTerminal: React.FC<LogsTerminalProps> = ({
           </Dropdown>
         </div>
         <div className="flex flex-row items-center gap-4">
-          <span>Follow</span>
           <CopyIcon height={30} width={30} />
           <DownloadAltIcon height={30} width={30} />
         </div>
       </div>
-      <div></div>
+      <hr
+        className={`
+          h-[1px]
+          w-full
+          ${variant === "light" ? "border-primary-600" : "border-nuances-300"}
+        `}
+      />
+      <div className="flex flex-row items-center gap-1 px-4 pt-4">
+        <AttemptButton
+          variant={variant}
+          attempt={1}
+          isActive={true}
+          onClick={() => console.log("active")}
+          onClose={() => console.log("close")}
+        />
+        <AttemptButton
+          variant={variant}
+          attempt={2}
+          isActive={false}
+          onClick={() => console.log("active")}
+          onClose={() => console.log("close")}
+        />
+      </div>
     </div>
   );
 };
