19f89047ac4b6b5fddb130d153ee0503fbdf9ec8
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-01-25 23:45:53 +0100

    refactor(ui): rewrite client functions with modern javascript

--- ui/src/clients/repositories/client.ts
+++ ui/src/clients/repositories/client.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 import { Repositories } from "@/clients/repositories/types.ts";
 
-export async function fetchRepositories(): Promise<Repositories> {
+export const fetchRepositories = async () => {
   const response = await axios.get<Repositories>(
     `${import.meta.env.VITE_API_BASE_URL}/repositories`
   );
   return response.data;
-}
+};
