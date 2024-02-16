4216d935a2fd384f5d9a62138d4def33dedf102b
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-02-16 16:50:30 +0100

    feat(ui): add openinlogs button

---
+++ ui/src/components/buttons/OpenInLogsButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Button from "@/components/core/Button";
+import ArrowResizeDiagonalIcon from "@/assets/icons/ArrowResizeDiagonalIcon";
+
+export interface OpenInLogsButtonProps {
+  className?: string;
+  variant?: "primary" | "secondary" | "tertiary";
+  onClick?: () => void;
+}
+
+const OpenInLogsButton: React.FC<OpenInLogsButtonProps> = ({
+  className,
+  variant = "primary",
+  onClick,
+}) => {
+  return (
+    <Button
+      rightIcon={<ArrowResizeDiagonalIcon />}
+      className={className}
+      variant={variant}
+      onClick={onClick}
+    >
+      Open in logs
+    </Button>
+  );
+};
+
+export default OpenInLogsButton;
