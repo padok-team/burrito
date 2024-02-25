f96d86ff387de72a29ec20c63b031c3c270041aa
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-02-25 14:39:58 +0100

    refactor(ui): rewrite states dropdown

---
+++ ui/src/components/dropdowns/StatesDropdown.tsx
@@ -0,0 +1,183 @@
+import React, { useState, useRef } from "react";
+import { twMerge } from "tailwind-merge";
+import {
+  useFloating,
+  useClick,
+  useDismiss,
+  useRole,
+  useListNavigation,
+  useTypeahead,
+  useInteractions,
+  FloatingFocusManager,
+  offset,
+  autoUpdate,
+  FloatingPortal,
+} from "@floating-ui/react";
+
+import Dropdown from "@/components/core/Dropdown";
+import Checkbox from "@/components/core/Checkbox";
+
+import { LayerState } from "@/clients/layers/types";
+
+export interface StatesDropdownProps {
+  className?: string;
+  variant?: "light" | "dark";
+  disabled?: boolean;
+  selectedStates: LayerState[];
+  setSelectedStates: (states: LayerState[]) => void;
+}
+
+const options: Array<{ value: LayerState; label: string }> = [
+  { value: "success", label: "OK" },
+  { value: "warning", label: "OutOfSync" },
+  { value: "error", label: "Error" },
+];
+
+const StatesDropdown: React.FC<StatesDropdownProps> = ({
+  className,
+  variant = "light",
+  disabled,
+  selectedStates,
+  setSelectedStates,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const listElementsRef = useRef<Array<HTMLElement | null>>([]);
+  const listContentRef = useRef<Array<string | null>>([]);
+  const isTypingRef = useRef(false);
+
+  const { refs, floatingStyles, context } = useFloating<HTMLElement>({
+    placement: "bottom-start",
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [offset(8)],
+  });
+
+  const click = useClick(context, {
+    enabled: !disabled,
+    event: "mousedown",
+  });
+  const listNavigation = useListNavigation(context, {
+    enabled: !disabled,
+    listRef: listElementsRef,
+    activeIndex: activeIndex,
+    onNavigate: setActiveIndex,
+  });
+  const typeahead = useTypeahead(context, {
+    enabled: !disabled,
+    listRef: listContentRef,
+    activeIndex: activeIndex,
+    onMatch: setActiveIndex,
+    onTypingChange(isTyping) {
+      isTypingRef.current = isTyping;
+    },
+  });
+  const dismiss = useDismiss(context);
+  const role = useRole(context, { role: "select" });
+
+  const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions(
+    [click, listNavigation, typeahead, dismiss, role]
+  );
+
+  const handleSelect = (state: LayerState) => {
+    if (selectedStates.includes(state)) {
+      setSelectedStates(selectedStates.filter((s) => s !== state));
+    } else {
+      setSelectedStates([...selectedStates, state]);
+    }
+  };
+
+  const styles = {
+    light: `bg-nuances-white
+      text-primary-600
+      shadow-light`,
+    dark: `bg-nuances-black
+      text-nuances-300
+      shadow-dark`,
+  };
+
+  return (
+    <>
+      <Dropdown
+        className={className}
+        label="States"
+        filled={selectedStates.length > 0}
+        disabled={disabled}
+        variant={variant}
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      />
+      {isOpen && (
+        <FloatingPortal>
+          <FloatingFocusManager context={context} modal={false}>
+            <div
+              ref={refs.setFloating}
+              style={floatingStyles}
+              className={twMerge(
+                `overflow-y-auto
+                rounded-lg
+                outline-none
+                px-4
+                py-2`,
+                styles[variant]
+              )}
+              {...getFloatingProps()}
+            >
+              <span className="font-semibold">States</span>
+              <hr
+                className={`
+                  h-[1px]
+                  w-auto
+                  -mx-4
+                  my-2
+                  ${
+                    variant === "light"
+                      ? "border-primary-600"
+                      : "border-nuances-300"
+                  }
+                `}
+              />
+              <div className="flex flex-col gap-1">
+                {options.map(({ value, label }, index) => (
+                  <Checkbox
+                    key={value}
+                    role="option"
+                    variant={variant}
+                    label={label}
+                    checked={selectedStates.includes(value)}
+                    readOnly
+                    tabIndex={activeIndex === index ? 0 : -1}
+                    forwardedRef={(node) => {
+                      listElementsRef.current[index] = node;
+                      listContentRef.current[index] = label;
+                    }}
+                    {...getItemProps({
+                      onClick() {
+                        handleSelect(value);
+                      },
+                      onKeyDown(event) {
+                        if (event.key === "Enter") {
+                          event.preventDefault();
+                          handleSelect(value);
+                        }
+
+                        if (event.key === " " && !isTypingRef.current) {
+                          event.preventDefault();
+                          handleSelect(value);
+                        }
+                      },
+                    })}
+                  />
+                ))}
+              </div>
+            </div>
+          </FloatingFocusManager>
+        </FloatingPortal>
+      )}
+    </>
+  );
+};
+
+export default StatesDropdown;
