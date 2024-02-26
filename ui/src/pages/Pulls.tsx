1ddf0fabe1fca2a6c5facdc0dddb5df004c8d51d
Marc-Antoine Godde <marc-antoine.godde@hotmail.fr>
2024-02-26 22:44:30 +0100

    style(ui): lint pulls page

--- ui/src/pages/Pulls.tsx
+++ ui/src/pages/Pulls.tsx
@@ -8,12 +8,14 @@ const Pulls: React.FC = () => {
   const { theme } = useContext(ThemeContext);
   return (
     <div className="flex flex-grow items-center justify-center p-8">
-      <div className="flex items-center gap-8  ">
+      <div className="flex items-center gap-8">
         <TrafficCone height={80} width={80} />
         <span
-          className={`text-5xl font-bold ${
-            theme === "light" ? "text-nuances-black" : "text-nuances-50"
-          }`}
+          className={`
+            text-5xl
+            font-bold
+            ${theme === "light" ? "text-nuances-black" : "text-nuances-50"}
+          `}
         >
           Pull requests page
         </span>
