89e8aebef986e38b2829243c6db2216ea8a1afe8
Lucas Marques <lucas.marques@theodo.com>
2024-08-30 17:32:06 +0200

    feat(server): add api route to add sync-now annotation

---
+++ internal/server/api/sync.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	configv1alpha1 "github.com/padok-team/burrito/api/v1alpha1"
+	"github.com/padok-team/burrito/internal/annotations"
+	log "github.com/sirupsen/logrus"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (a *API) SyncLayerHandler(c echo.Context) error {
+	layer := &configv1alpha1.TerraformLayer{}
+	err := a.Client.Get(context.Background(), client.ObjectKey{
+		Namespace: c.Param("namespace"),
+		Name:      c.Param("layer"),
+	}, layer)
+	if err != nil {
+		log.Errorf("could not get terraform layer: %s", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "An error occurred while getting the layer"})
+	}
+	err = annotations.Add(context.Background(), a.Client, layer, map[string]string{
+		annotations.SyncNow: "true",
+	})
+	if err != nil {
+		log.Errorf("could not update terraform layer annotations: %s", err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "An error occurred while updating the layer annotations"})
+	}
+	return c.JSON(http.StatusOK, map[string]string{"status": "Layer sync triggered"})
+}
