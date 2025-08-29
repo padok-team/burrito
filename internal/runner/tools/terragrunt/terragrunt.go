a3da7fc2148bec7f13dfa6bfd02be57815a49f48
Michael Todorovic <michael.todorovic@outlook.com>
2025-08-29 15:12:55 +0200

    fix: handle terragrunt 0.73.0+ new flags (#662)
    Signed-off-by: Michael Todorovic <michael.todorovic@outlook.com>

--- internal/runner/tools/terragrunt/terragrunt.go
+++ internal/runner/tools/terragrunt/terragrunt.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"os/exec"
 
+	"github.com/blang/semver/v4"
 	c "github.com/padok-team/burrito/internal/utils/cmd"
 )
 
@@ -11,6 +12,7 @@ type Terragrunt struct {
 	ExecPath      string
 	WorkingDir    string
 	ChildExecPath string
+	Version       string
 }
 
 func (t *Terragrunt) TenvName() string {
@@ -18,14 +20,46 @@ func (t *Terragrunt) TenvName() string {
 }
 
 func (t *Terragrunt) getDefaultOptions(command string) ([]string, error) {
-	return []string{
-		command,
-		"--terragrunt-tfpath",
-		t.ChildExecPath,
-		"--terragrunt-working-dir",
-		t.WorkingDir,
-		"-no-color",
-	}, nil
+	// Parse the version to determine which flags to use
+	// Terragrunt 0.73.0 introduced new shortened flags:
+	// - --terragrunt-tfpath -> --tf-path
+	// - --terragrunt-working-dir -> --working-dir
+	version, err := semver.Parse(t.Version)
+	if err != nil {
+		// If version parsing fails, use legacy flags as fallback
+		return []string{
+			command,
+			"--terragrunt-tfpath",
+			t.ChildExecPath,
+			"--terragrunt-working-dir",
+			t.WorkingDir,
+			"-no-color",
+		}, nil
+	}
+
+	newFlagsVersion := semver.MustParse("0.73.0")
+
+	if version.GTE(newFlagsVersion) {
+		// Use new flags for version 0.73.0 and above
+		return []string{
+			command,
+			"--tf-path",
+			t.ChildExecPath,
+			"--working-dir",
+			t.WorkingDir,
+			"-no-color",
+		}, nil
+	} else {
+		// Use legacy flags for versions below 0.73.0
+		return []string{
+			command,
+			"--terragrunt-tfpath",
+			t.ChildExecPath,
+			"--terragrunt-working-dir",
+			t.WorkingDir,
+			"-no-color",
+		}, nil
+	}
 }
 
 func (t *Terragrunt) Init(workingDir string) error {
