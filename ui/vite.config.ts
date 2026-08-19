6a85b46deffe325b34356d99c3c556f1c5aee84a
Stevie Gayet <stevie.gayet@backmarket.com>
2026-08-19 21:47:32 +0200

    chore: switch to `oxlint` and `oxfmt`

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      "@": "/src",
-    },
-  },
+      '@': '/src'
+    }
+  }
 });
