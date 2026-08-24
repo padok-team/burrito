e98cd8173e1bdd6924663a53fa5984a733e4d895
vedantoncloud <vedantmanikmishra@gmail.com>
2026-08-24 21:14:13 +0530

    feat: add non-destructive apply support

--- internal/utils/runner/plan_diff.go
+++ internal/utils/runner/plan_diff.go
@@ -6,11 +6,16 @@ import (
 	tfjson "github.com/hashicorp/terraform-json"
 )
 
-// Produces a diff summary from the given plan
-func GetDiff(plan *tfjson.Plan) (bool, string) {
+// Produces a diff summary from the given plan.
+// Returns:
+//   - whether the plan has any changes
+//   - whether the plan contains destructive changes
+//   - a human-readable summary
+func GetDiff(plan *tfjson.Plan) (bool, bool, string) {
 	delete := 0
 	create := 0
 	update := 0
+
 	for _, res := range plan.ResourceChanges {
 		if res.Change.Actions.Create() {
 			create++
@@ -26,9 +31,14 @@ func GetDiff(plan *tfjson.Plan) (bool, string) {
 			delete++
 		}
 	}
-	diff := false
-	if create+delete+update > 0 {
-		diff = true
-	}
-	return diff, fmt.Sprintf("Plan: %d to create, %d to update, %d to delete", create, update, delete)
+
+	diff := create+delete+update > 0
+	destructive := delete > 0
+
+	return diff, destructive, fmt.Sprintf(
+		"Plan: %d to create, %d to update, %d to delete",
+		create,
+		update,
+		delete,
+	)
 }
